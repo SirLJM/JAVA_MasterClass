9d191728e5fed7d3b9f7bb9d2df91b92453723f5
Lucas <spambigbox@gmail.com>
2018-09-09 15:33:39 +0200

    LastDigitChecker done

--- LastDigitChecker/src/com/company/Main.java
+++ LastDigitChecker/src/com/company/Main.java
@@ -3,6 +3,23 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        System.out.println(hasSameLastDigit(41, 22, 71));
+        System.out.println(hasSameLastDigit(23, 32, 42));
+        System.out.println(hasSameLastDigit(9, 99, 999));
+    }
+
+    public static boolean hasSameLastDigit(int first, int second, int third) {
+        if (first < 10 || first > 1000 || second < 10 || second > 1000 || third < 10 || third > 1000) {
+            return false;
+        }
+
+        int firstLastDigit = first % 10;
+        int secondLastDigit = second % 10;
+        int thirdLastDigit = third % 10;
+
+        return firstLastDigit == secondLastDigit ||
+                firstLastDigit == thirdLastDigit ||
+                secondLastDigit == thirdLastDigit;
     }
 }
