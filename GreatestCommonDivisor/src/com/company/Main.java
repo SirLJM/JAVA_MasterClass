b5ee8b0cc4b631aedcaedd0695a42db56e10127e
Unknown <spambigbox@gmail.com>
2018-10-12 14:22:23 +0200

    GreatestCommonDivisor done

--- GreatestCommonDivisor/src/com/company/Main.java
+++ GreatestCommonDivisor/src/com/company/Main.java
@@ -1,8 +1,23 @@
 package com.company;
 
+import java.math.BigInteger;
+
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        System.out.println(getGreatestCommonDivisor(12,30));
+        System.out.println(getGreatestCommonDivisor(25,15));
+        System.out.println(getGreatestCommonDivisor(9,18));
+        System.out.println(getGreatestCommonDivisor(81,153));
+
+    }
+
+    public static int getGreatestCommonDivisor(int first, int second) {
+        if (first < 10 || second < 10) {
+            return -1;
+        }
+        return BigInteger.valueOf(first).gcd(BigInteger.valueOf(second)).intValue();
     }
 }
