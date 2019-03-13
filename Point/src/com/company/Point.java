30e2ae14fdaec1c96c4072891741b2f0fad7d9c8
Unknown <spambigbox@gmail.com>
2019-03-13 14:26:31 +0100

    Point done

---
+++ Point/src/com/company/Point.java
@@ -0,0 +1,46 @@
+package com.company;
+
+public class Point {
+
+    private int x;
+    private int y;
+
+    public Point() {
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public double distance(int x, int y) {
+        return Math.sqrt((x - this.x)*(x - this.x) + (y - this.y)*(y - this.y));
+    }
+
+    public double distance() {
+        return distance(0,0);
+    }
+
+    public double distance(Point point) {
+        return distance(point.getX(), point.getY());
+    }
+
+}
