91a477f7de8a897dcdfc93a072a1fe04a6aec450
Lucas <spambigbox@gmail.com>
2019-09-24 14:44:53 +0200

    ArrayChallenge done

---
+++ ArrayChallenge/src/com/company/Main.java
@@ -0,0 +1,53 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        int[] array = getIntegers(4);
+        int[] sorted = sortIntegers(array);
+        printArray(sorted);
+    }
+
+    public static int[] getIntegers(int number) {
+        int[] array = new int[number];
+        System.out.println("Type in " + number + " values: \r");
+        for (int i = 0; i < array.length; i++) {
+            array[i] = scanner.nextInt();
+        }
+        return array;
+    }
+
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Element " + i + ", value is " + array[i]);
+        }
+    }
+
+    public static int[] sortIntegers(int[] array) {
+//        int[] sortedArray = new int[array.length];
+//        for (int i = 0; i < array.length; i++) {
+//            sortedArray[i] = array[i];
+//        }
+        int[] sortedArray = Arrays.copyOf(array, array.length);
+        boolean flag = true;
+        int temp;
+        while (flag) {
+            flag = false;
+            for (int i = 0; i < sortedArray.length - 1; i++) {
+                if (sortedArray[i] < sortedArray[i+1]) {
+                    temp = sortedArray[i];
+                    sortedArray[i] = sortedArray[i+1];
+                    sortedArray[i+1] = temp;
+                    flag = true;
+                 }
+            }
+        }
+        return sortedArray;
+    }
+}
