a2c1ca3b1b3ee5be2eea2336a45f797452073827
Lucas <spambigbox@gmail.com>
2019-10-01 13:27:52 +0200

    MinElementChaleenge done

---
+++ MinElementChallenge/src/com/company/Main.java
@@ -0,0 +1,36 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        System.out.println("Give number of elements: ");
+        int count = scanner.nextInt();
+        int[] array = readIntegers(count);
+        System.out.println("Minimum number is: " + findMin(array));
+    }
+
+    public static int[] readIntegers(int count) {
+        int[] array = new int[count];
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Give element " + i + " :");
+            array[i] = scanner.nextInt();
+        }
+        return array;
+    }
+
+    public static int findMin(int[] array) {
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < array.length; i++) {
+            int value = array[i];
+            if (value < min) {
+                min = value;
+            }
+        }
+        return min;
+    }
+}
